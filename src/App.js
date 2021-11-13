32b558a1008ae4bd4c6aa46d5bfa3b6a62f8beae
Thalles Yuri <thalles.yuri1995@gmail.com>
2021-11-13 05:09:19 -0800

    Adding routing action to the web site using react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+//Importing the routes that's going to surround the each route
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
+import HomePage from './pages/HomePage/HomePage.component';
+
+const HatsPage = () => {
+    return (
+        <div>
+            <h1>HATS PAGE</h1>
+        </div>
+    );
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        //Inside the Route: "exact" is to force the path to match the path
+        //"path" is the "address" that you woul like to use
+        //"element" is the element the component used
+        <div>
+            <Routes>
+                <Route exact path="/" element={<HomePage />} />
+                <Route path="/shop/hats" element={<HatsPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/menu-item/MenuItem.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './MenuItem.styles.scss';
+import { useNavigate } from 'react-router';
+
+//Importing the access to router control inside this component
+
+//Finally accessing the history inside the props of react-router-dom
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    const navigate = useNavigate();
+    return (
+        //Pushing the link for the other page in the router --
+        <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+            ></div>
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    );
+};
+
+//This will give the power to the component of access the others routes
+export default MenuItem;
